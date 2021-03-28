61654991b328aaf6bb0e670864215f05e26a269a
FeMiqui <51029840+FeMiqui@users.noreply.github.com>
2021-03-27 22:51:36 -0300

    Create Script.js
    Script and how not to forget

---
+++ Script.js
@@ -0,0 +1,11 @@
+function getMyFolders() {
+ const folders = DriveApp.getFolders();
+ /*or Files:  const files = DriveApp.getFiles();*/
+ var i = 0;
+ while (folders.hasNext()){ /*iterating through the docs/folders/files*/
+   var folder = folders.next(); 
+     for (i=0;i< folder.getEditors().length; i++){ /*Has to check if the editor is there, otherwise it might scan a file where he isn't there and throw an error*/
+       if (folder.getEditors()[i].getEmail() == 'person@email.com'){folder.removeEditor('person@email.com');}
+     }
+  }
+}
